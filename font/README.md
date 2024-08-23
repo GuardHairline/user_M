4ec28098484e0150f9e39df1a477fda63f8acf71
lx <445462184@qq.com>
2024-08-23 16:27:07 +0800

    按钮授权，接口完善，readme简介

---
+++ font/README.md
@@ -0,0 +1,1 @@
+执行 `npm run dev `命令，启动项目，通过 [http://localhost:8090](http://localhost:8090/) 访问。
---
+++ font/src/http/api.js
@@ -0,0 +1,18 @@
+/* 
+ * 接口统一集成模块
+ */
+import * as login from './moudules/login'
+import * as user from './moudules/user'
+import * as dept from './moudules/dept'
+import * as role from './moudules/role'
+import * as menu from './moudules/menu'
+
+
+// 默认全部导出
+export default {
+    login,
+    user,
+    dept,
+    role,
+    menu,
+}
